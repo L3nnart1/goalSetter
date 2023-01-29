2bc045f9cbd9380894a8c8efded77f3e9007412b
L3nnart1 <lobitz.business@gmail.com>
2023-01-29 13:50:39 +0100

    bavk

--- airbnb/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "./style.css";
-import App from "./components/app";
-
-ReactDom.render(<App />, document.getElementById("root"));
